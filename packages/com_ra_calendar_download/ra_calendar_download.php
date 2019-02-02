206aa939505727ab65e930aa1dd59fd0b0828474
Keith Grimes <keith.grimes@kcgrimes.co.uk>
2019-02-02 23:33:30 +0000

    Logging added to com_ra_calendar_download
    Logging added to support development. Information into a dedicated log
    file.

--- packages/com_ra_calendar_download/ra_calendar_download.php
+++ packages/com_ra_calendar_download/ra_calendar_download.php
@@ -13,6 +13,19 @@ JLoader::register('ra_calendar_downloadHelper', dirname(__FILE__).
 
 jimport('joomla.application.component.controller');
 
+JLog::addLogger(
+       array(
+            // Sets file name
+            'text_file' => 'pkg_ra_calendar_download.log.php'
+       ),
+           // Sets messages of all log levels to be sent to the file
+       JLog::ALL,
+           // The log category/categories which should be recorded in this file
+           // In this case, it's just the one category from our extension, still
+           // we need to put it inside an array
+       array('com_ra_calendar_download', 'mod_ra_calendar_download')
+   );
+
 $controller = JControllerLegacy::getInstance('ra_calendar_download');
 $jinput = JFactory::getApplication()->input;
 $task = $jinput->get('task', "", 'STR');
